5a99af1897946a63dd9834c4a33ed0bfe6502cad
Talha Kaan Özkan <talhakaanozkan@gmail.com>
2024-02-24 17:11:59 +0300

    update webhook clerk

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,16 +1,18 @@
-import { Webhook } from 'svix'
-import { headers } from 'next/headers'
-import { WebhookEvent } from '@clerk/nextjs/server'
+import { Webhook } from "svix";
+import { headers } from "next/headers";
+import { WebhookEvent } from "@clerk/nextjs/server";
 
-import { db } from '@/lib/db'
-import { resetIngresses } from '@/actions/ingress'
+import { db } from "@/lib/db";
+import { resetIngresses } from "@/actions/ingress";
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-  const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET
- 
+  const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET;
+
   if (!WEBHOOK_SECRET) {
-    throw new Error('Please add CLERK_WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
+    throw new Error(
+      "Please add CLERK_WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
+    );
   }
 
   // Get the headers
@@ -21,19 +23,19 @@ export async function POST(req: Request) {
 
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response('Error occured -- no svix headers', {
-      status: 400
-    })
+    return new Response("Error occured -- no svix headers", {
+      status: 400,
+    });
   }
 
   // Get the body
-  const payload = await req.json()
+  const payload = await req.json();
   const body = JSON.stringify(payload);
 
   // Create a new Svix instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
 
-  let evt: WebhookEvent
+  let evt: WebhookEvent;
 
   // Verify the payload with the headers
   try {
@@ -41,12 +43,12 @@ export async function POST(req: Request) {
       "svix-id": svix_id,
       "svix-timestamp": svix_timestamp,
       "svix-signature": svix_signature,
-    }) as WebhookEvent
+    }) as WebhookEvent;
   } catch (err) {
-    console.error('Error verifying webhook:', err);
-    return new Response('Error occured', {
-      status: 400
-    })
+    console.error("Error verifying webhook:", err);
+    return new Response("Error occured", {
+      status: 400,
+    });
   }
 
   const eventType = evt.type;
@@ -55,11 +57,11 @@ export async function POST(req: Request) {
     await db.user.create({
       data: {
         externalUserId: payload.data.id,
-        username: payload.data.username,
+        username: payload.data.username ?? payload.data.first_name ?? evt.data.email_addresses[0].email_address,
         imageUrl: payload.data.image_url,
         stream: {
           create: {
-            name: `${payload.data.username}'s stream`,
+            name: `${payload.data.username ?? payload.data.first_name ?? evt.data.email_addresses[0].email_address}'s stream`,
           },
         },
       },
@@ -72,12 +74,12 @@ export async function POST(req: Request) {
         externalUserId: payload.data.id,
       },
       data: {
-        username: payload.data.username,
+        username: payload.data.username ?? payload.data.first_name ?? evt.data.email_addresses[0].email_address,
         imageUrl: payload.data.image_url,
       },
     });
   }
- 
+
   if (eventType === "user.deleted") {
     await resetIngresses(payload.data.id);
 
@@ -87,6 +89,6 @@ export async function POST(req: Request) {
       },
     });
   }
- 
-  return new Response('', { status: 200 })
-};
+
+  return new Response("", { status: 200 });
+}
